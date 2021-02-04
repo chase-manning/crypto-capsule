d172c5706b7411687cee164644165921fff59a42
Chase Manning <me@chasemanning.co.nz>
2021-02-04 20:27:30 +1300

    :rocket: add footer

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Footer from "./Footer";
 import Header from "./Header";
 import Landing from "./Landing";
 
@@ -14,6 +15,7 @@ const App = () => {
     <StyledApp>
       <Header />
       <Landing />
+      <Footer />
     </StyledApp>
   );
 };
