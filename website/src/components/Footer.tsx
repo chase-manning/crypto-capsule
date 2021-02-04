3858b641877eb89fc02e7222435b9b58e44a5cee
Chase Manning <me@chasemanning.co.nz>
2021-02-04 20:27:24 +1300

    :sparkles: create footer component

---
+++ website/src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  width: 100%;
+  padding: 35px 60px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Text = styled.div`
+  font-size: 14px;
+`;
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <div />
+      <Text>{"Made with ❤️in New Zealand"}</Text>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
