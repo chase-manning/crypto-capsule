feec573325bd2863d21f5d52bcbe31bf9314fb6a
Chase Manning <me@chasemanning.co.nz>
2021-02-04 20:21:46 +1300

    :rocket: add landing page header

--- website/src/components/Landing.tsx
+++ website/src/components/Landing.tsx
@@ -6,10 +6,31 @@ const StyledLanding = styled.div`
   flex: 1;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  margin: 0 150px;
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+`;
+
+const Header = styled.h1`
+  font-size: 80px;
+  font-weight: 700;
+  color: var(--main);
 `;
 
 const Landing = () => {
-  return <StyledLanding>meow</StyledLanding>;
+  return (
+    <StyledLanding>
+      <Left>
+        <Header>Crypto Capsule the Blockchain Time Capsule</Header>
+      </Left>
+      <div />
+    </StyledLanding>
+  );
 };
 
 export default Landing;
