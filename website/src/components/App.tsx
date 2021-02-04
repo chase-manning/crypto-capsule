8cf97df92c9a871e1188046460929045389c0d49
Chase Manning <me@chasemanning.co.nz>
2021-02-04 19:41:04 +1300

    :fire: remove boilerplate

---
+++ website/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  color: red;
+`;
+
+const App = () => {
+  return <StyledApp>Meow</StyledApp>;
+};
+
+export default App;
