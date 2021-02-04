cd3d793f0cf6562860b0011d9f5b312536e42207
Chase Manning <me@chasemanning.co.nz>
2021-02-04 19:46:30 +1300

    :rocket: add global styles to index

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -4,10 +4,12 @@ import App from "./components/App";
 import { store } from "./state/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>,
