3ad4592a6e527f1b01e89bca6098c3ee943b91d1
Chase Manning <me@chasemanning.co.nz>
2021-02-04 20:15:16 +1300

    :sparkles: create landing component

---
+++ website/src/components/Landing.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLanding = styled.div`
+  width: 100%;
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Landing = () => {
+  return <StyledLanding>meow</StyledLanding>;
+};
+
+export default Landing;
