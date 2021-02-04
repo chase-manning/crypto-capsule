3bcfaf603be77502ff1fefde7ae3036bb3dfdc11
Chase Manning <me@chasemanning.co.nz>
2021-02-04 20:13:00 +1300

    :sparkles: create color variables

--- website/src/styles/GlobalStyles.tsx
+++ website/src/styles/GlobalStyles.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    :root {
+        --main: #223444;
+        --sub: #98999B;
+        --primary: #1661F4;
+        --dark: #1C2326;
+    }
+
     * {
       box-sizing: border-box;
       margin: 0;
