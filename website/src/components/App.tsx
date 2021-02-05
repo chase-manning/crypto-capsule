8fe340a662891eece1cd105c98fa290e834cfafd
Chase Manning <me@chasemanning.co.nz>
2021-02-06 09:07:00 +1300

    :rocket: add routing for sent page

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CreatePage from "./CreatePage";
+import SentPage from "./SentPage";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        {/* <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/users">
-        <Users />
-      </Route> */}
+        <Route path="/sent">
+          <SentPage />
+        </Route>
         <Route path="/">
           <CreatePage />
         </Route>
