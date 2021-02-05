bd97083b34a9b04811c4d968c3c2fc1bd4bf0ea0
Chase Manning <me@chasemanning.co.nz>
2021-02-06 09:09:24 +1300

    :sparkles: create about page

---
+++ website/src/components/AboutPage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+
+const StyledAboutPage = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const AboutPage = () => {
+  return (
+    <StyledAboutPage>
+      <Header />
+      meow
+    </StyledAboutPage>
+  );
+};
+
+export default AboutPage;
