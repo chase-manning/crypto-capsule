b244b2ada37fc691b6c85f9141fc2584fed9100d
Chase Manning <me@chasemanning.co.nz>
2021-02-06 09:03:30 +1300

    :bug: broken routing

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CreatePage from "./CreatePage";
 
 const App = () => {
   return (
-    <Switch>
-      {/* <Route path="/about">
+    <Router>
+      <Switch>
+        {/* <Route path="/about">
         <About />
       </Route>
       <Route path="/users">
         <Users />
       </Route> */}
-      <Route path="/">
-        <CreatePage />
-      </Route>
-    </Switch>
+        <Route path="/">
+          <CreatePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
