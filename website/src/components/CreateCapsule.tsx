6f1d9533eed26faa92e4c1add18fcc47f0ff9f1e
Chase Manning <me@chasemanning.co.nz>
2021-02-06 22:30:51 +1300

    :lipstick: remove create capsule shadow

--- website/src/components/CreateCapsule.tsx
+++ website/src/components/CreateCapsule.tsx
@@ -8,7 +8,7 @@ const StyledCreateCapsule = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.1);
+  /* background-color: rgba(0, 0, 0, 0.1); */
   display: flex;
   justify-content: center;
   align-items: center;
