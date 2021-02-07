82765a77f3767b912039cbcf99eb06cba8ca8c8a
Chase Manning <me@chasemanning.co.nz>
2021-02-08 12:41:55 +1300

    :fire: remove redundant import

--- client/src/components/CreatePage.tsx
+++ client/src/components/CreatePage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import getWeb3 from "../services/web3Service";
 import Footer from "./Footer";
