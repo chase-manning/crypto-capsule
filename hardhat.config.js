7eb3b8cab87271bd75f558c39a44e1710f30f532
Chase Manning <me@chasemanning.co.nz>
2021-04-15 21:44:36 +1200

    :wrench: add hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+var secrets = require("./secrets");
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/" + secrets.alchemyKey,
+      accounts: [secrets.walletPrivateKey],
+    },
+  },
+};
