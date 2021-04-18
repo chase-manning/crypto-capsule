3fa0f11dff2083dc5be41f9432b2108b51c7efd9
Chase Manning <me@chasemanning.co.nz>
2021-04-18 13:26:30 +1200

    :art: remove params from create page

--- client/src/components/CreatePage.tsx
+++ client/src/components/CreatePage.tsx
@@ -3,8 +3,6 @@ import styled from "styled-components";
 import Footer from "./Footer";
 import Header from "./Header";
 import Landing from "./Landing";
-import Web3 from "web3";
-import { Contract } from "web3-eth-contract";
 
 const StyledCreatePage = styled.div`
   height: 100vh;
@@ -12,15 +10,11 @@ const StyledCreatePage = styled.div`
   flex-direction: column;
 `;
 
-type Props = {
-  web3?: Web3;
-  capsuleFactory?: Contract;
-};
-const CreatePage = (props: Props) => {
+const CreatePage = () => {
   return (
     <StyledCreatePage>
       <Header />
-      <Landing web3={props.web3} capsuleFactory={props.capsuleFactory} />
+      <Landing />
       <Footer />
     </StyledCreatePage>
   );
