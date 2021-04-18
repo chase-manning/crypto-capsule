0ad3f771778ff40b3f52a93d9231a78e1f4ce44a
Chase Manning <me@chasemanning.co.nz>
2021-04-18 13:25:00 +1200

    :sparkles: add capsule type

---
+++ client/src/types/CapsuleType.ts
@@ -0,0 +1,12 @@
+type CapsuleType = {
+  grantor: string;
+  beneficiary: string;
+  distributionDate: number;
+  createdDate: number;
+  opened: boolean;
+  value: number;
+  tokens: string[];
+  values: number[];
+};
+
+export default CapsuleType;
