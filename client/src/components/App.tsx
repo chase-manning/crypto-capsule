bc031d6997e3acef74d78dc349af1d14a1b8991b
Chase Manning <me@chasemanning.co.nz>
2021-04-18 13:25:41 +1200

    :art: change app to use new web3 service

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,44 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AboutPage from "./AboutPage";
 import CreatePage from "./CreatePage";
 import ReceivedPage from "./ReceivedPage";
 import SentPage from "./SentPage";
-import Web3 from "web3";
-import getWeb3 from "../services/web3Service";
-import CapsuleFactory from "../contracts/CryptoCapsule.json";
-import { useDispatch } from "react-redux";
-import { setAccounts } from "../state/web3Slice";
-import { Contract } from "web3-eth-contract";
+import { initWeb3 } from "../services/web3Service";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const [web3, setWeb3] = useState<Web3 | undefined>();
-  const [capsuleFactory, setCapsuleFactory] = useState<Contract | undefined>();
-
-  const initWeb3 = async () => {
-    try {
-      const _web3 = await getWeb3();
-      const _accounts = await _web3.eth.getAccounts();
-      const networkId = await _web3.eth.net.getId();
-      const deployedNetwork = (CapsuleFactory as any).networks[networkId];
-      const _instance = new _web3.eth.Contract(
-        (CapsuleFactory as any).abi,
-        deployedNetwork && deployedNetwork.address
-      );
-
-      setWeb3(_web3);
-      setCapsuleFactory(_instance);
-      dispatch(setAccounts(_accounts));
-    } catch (error) {
-      alert("Error Loading Web3");
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
     initWeb3();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -54,7 +24,7 @@ const App = () => {
           <AboutPage />
         </Route>
         <Route path="/">
-          <CreatePage web3={web3} capsuleFactory={capsuleFactory} />
+          <CreatePage />
         </Route>
       </Switch>
     </Router>
