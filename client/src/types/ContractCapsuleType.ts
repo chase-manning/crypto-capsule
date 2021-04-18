77e1c620e35c0d90e053f14ba4a8eb5debbce814
Chase Manning <me@chasemanning.co.nz>
2021-04-18 15:21:02 +1200

    :sparkles: create contract capsule type

---
+++ client/src/types/ContractCapsuleType.ts
@@ -0,0 +1,12 @@
+type ContractCapsuleType = {
+  grantor: string;
+  beneficiary: string;
+  distributionDate: string;
+  createdDate: string;
+  opened: boolean;
+  value: string;
+  tokens: string[];
+  values: string[];
+};
+
+export default ContractCapsuleType;
