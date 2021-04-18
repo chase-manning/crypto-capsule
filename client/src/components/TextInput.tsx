17ae3a0897bd08273a2bfa31778d4e9c53cf249e
Chase Manning <me@chasemanning.co.nz>
2021-04-18 19:49:20 +1200

    :sparkles: add tooltip to text input

--- client/src/components/TextInput.tsx
+++ client/src/components/TextInput.tsx
@@ -2,22 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import Input from "../styles/Input";
 import Label from "../styles/Label";
+import Tooltip from "./Tooltip";
 
 const StyledTextInput = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
+const LabelContainter = styled.div`
+  display: flex;
+`;
+
 type Props = {
   label: string;
   placeholder?: string;
   maxWidth?: string;
+  tooltip?: string;
 };
 
 const TextInput = (props: Props) => {
   return (
     <StyledTextInput>
-      <Label>{props.label}</Label>
+      <LabelContainter>
+        <Label>{props.label}</Label>
+        {props.tooltip && <Tooltip content={props.tooltip}></Tooltip>}
+      </LabelContainter>
       <Input placeholder={props.placeholder} maxWidth={props.maxWidth} />
     </StyledTextInput>
   );
