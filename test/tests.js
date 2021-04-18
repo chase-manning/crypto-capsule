4d82634dadf4388f096c8a2157d66af8907d8b49
Chase Manning <me@chasemanning.co.nz>
2021-04-18 14:39:57 +1200

    :rocket: add capsule creation tests

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,44 @@
+const { BigNumber } = require("@ethersproject/bignumber");
 const { expect } = require("chai");
+
+const SENDER_ADDRESS = "0x07d48BDBA7975f0DAF73BD5b85A2E3Ff87ffb24e";
+const RECEIVER_ADDRESS = "0xd83BbB419B928bB8066847f6c66eA453Fb062C7f";
 
 describe("Capsule", () => {
   it("Should successfully deply", async () => {
     const Capsule = await ethers.getContractFactory("CryptoCapsule");
     const capsule = await Capsule.deploy();
     await capsule.deployed();
+  });
+
+  it("Should have no Capsules on Creation", async () => {
+    const Capsule = await ethers.getContractFactory("CryptoCapsule");
+    const capsule = await Capsule.deploy();
+    await capsule.deployed();
+
+    const sent = await capsule.getSentCapsules(SENDER_ADDRESS);
+    expect(sent.length).to.equal(0);
+
+    const received = await capsule.getSentCapsules(RECEIVER_ADDRESS);
+    expect(received.length).to.equal(0);
+  });
+
+  it("Should Create Capsule", async () => {
+    const Capsule = await ethers.getContractFactory("CryptoCapsule");
+    const capsule = await Capsule.deploy();
+    await capsule.deployed();
+
+    await capsule.createCapsule(RECEIVER_ADDRESS, new Date().getTime(), [], []);
+
+    const capsuleMeow = await capsule.getCapsule(0);
+    console.log(capsuleMeow);
+
+    const sent = await capsule.getSentCapsules(SENDER_ADDRESS);
+    console.log(sent);
+    // expect(sent).to.equal([]);
+
+    const received = await capsule.getSentCapsules(RECEIVER_ADDRESS);
+    console.log(received);
+    // expect(received).to.equal([]);
   });
 });
