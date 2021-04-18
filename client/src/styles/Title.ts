196b2952b3667d6c30f2a9ff9d9cd2c5a50627f6
Chase Manning <me@chasemanning.co.nz>
2021-04-18 18:09:05 +1200

    :sparkles: create title styled component

---
+++ client/src/styles/Title.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h2`
+  color: var(--main);
+  font-size: 35px;
+  font-weight: 700;
+  margin-top: 70px;
+  margin-bottom: 20px;
+`;
+
+export default Title;
