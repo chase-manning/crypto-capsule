040ca381b965b9eee25dc9708697afd9b26d2dd8
Chase Manning <me@chasemanning.co.nz>
2021-04-18 13:24:52 +1200

    :sparkles: add globals file

---
+++ client/src/utils/globals.ts
@@ -0,0 +1,11 @@
+const TEST_MODE = true;
+
+type GlobalsType = {
+  CAPSULE: string;
+};
+
+const GLOBALS: GlobalsType = {
+  CAPSULE: "0x41B58E454bDbC9B204d9cE115a5fB83d4d86eF33",
+};
+
+export default GLOBALS;
