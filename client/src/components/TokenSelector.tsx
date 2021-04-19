46750a4ed473e0c83e23a639e4f3504d484c9d73
Chase Manning <me@chasemanning.co.nz>
2021-04-19 20:45:43 +1200

    :rocket: add ability to select tokens

--- client/src/components/TokenSelector.tsx
+++ client/src/components/TokenSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { selectTokens } from "../state/tokenSlice";
@@ -35,6 +35,40 @@ const ExitEvent = styled.div`
   background-color: rgba(0, 0, 0, 0.2);
 `;
 
+const Tokens = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const TokenContainer = styled.button`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  padding: 0.8rem;
+  cursor: pointer;
+  transition: all 0.2s;
+  border-radius: 0.8rem;
+
+  :hover {
+    background-color: var(--primary-light);
+  }
+`;
+
+const Image = styled.img`
+  width: 2.4rem;
+  margin-right: 2rem;
+  background-color: var(--white);
+  border-radius: 50%;
+`;
+
+const Name = styled.div`
+  color: var(--main);
+  font-size: 1.4rem;
+  font-weight: 500;
+  margin-right: 1rem;
+`;
+
 type Props = {
   open: boolean;
   token: Token;
@@ -43,12 +77,34 @@ type Props = {
 
 const TokenSelector = (props: Props) => {
   const tokens = useSelector(selectTokens);
+  const [search, setSearch] = useState("");
 
   return (
     <StyledTokenSelector show={props.open}>
       <ExitEvent onClick={() => props.setToken(props.token)} />
       <Container>
-        <Input placeholder="Search name or paste address" />
+        <Input
+          placeholder="Search name or paste address"
+          value={search}
+          onChange={(e: any) => setSearch(e.target.value)}
+        />
+        <Tokens>
+          {tokens
+            .filter((token: Token) => {
+              if (!search) return true;
+              return (
+                token.name.substring(0, search.length).toLowerCase() ===
+                search.toLowerCase()
+              );
+            })
+            .slice(0, 10)
+            .map((token: Token) => (
+              <TokenContainer onClick={() => props.setToken(token)}>
+                <Image src={token.logoURI} />
+                <Name>{token.name}</Name>
+              </TokenContainer>
+            ))}
+        </Tokens>
       </Container>
     </StyledTokenSelector>
   );
