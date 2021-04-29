aeb624d29085b3912cd6dd9c2d63358e2f3ecb1c
Chase Manning <me@chasemanning.co.nz>
2021-04-29 10:52:27 +1000

    :bug: rinkeby icons showing incorrectly

--- client/src/data/rinkebyTokens.ts
+++ client/src/data/rinkebyTokens.ts
@@ -7,7 +7,7 @@ const rinkebyTokens: Token[] = [
     symbol: "WETH",
     decimals: 18,
     logoURI:
-      "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xc778417E063141139Fce010982780140Aa0cD5Ab/logo.png",
+      "https://assets.coingecko.com/coins/images/279/thumb/ethereum.png?1595348880",
   },
   {
     name: "Dai Stablecoin",
@@ -15,7 +15,7 @@ const rinkebyTokens: Token[] = [
     symbol: "DAI",
     decimals: 18,
     logoURI:
-      "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735/logo.png",
+      "https://assets.coingecko.com/coins/images/9956/thumb/dai-multi-collateral-mcd.png?1574218774",
   },
   {
     name: "Maker",
@@ -23,14 +23,15 @@ const rinkebyTokens: Token[] = [
     symbol: "MKR",
     decimals: 18,
     logoURI:
-      "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85/logo.png",
+      "https://assets.coingecko.com/coins/images/1364/thumb/Mark_Maker.png?1585191826",
   },
   {
     name: "Uniswap",
     address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
     symbol: "UNI",
     decimals: 18,
-    logoURI: "ipfs://QmXttGpZrECX5qCyXbBQiqgQNytVGeZW5Anewvh2jc4psg",
+    logoURI:
+      "https://assets.coingecko.com/coins/images/12504/thumb/uniswap-uni.png?1600306604",
   },
 ];
 
