2c41bce32d37e20298db6724458312fa9e489619
Chase Manning <me@chasemanning.co.nz>
2021-04-30 14:31:08 +1000

    :wrench: update capsule address

--- client/src/utils/globals.ts
+++ client/src/utils/globals.ts
@@ -6,7 +6,7 @@ type GlobalsType = {
 };
 
 const GLOBALS: GlobalsType = {
-  CAPSULE: "0xBA3146D642E33692C5C01fFaE34B639ec85085B9",
+  CAPSULE: "0x14430C6E97d76029B1de0926303F88d90669Ac40",
   NETWORK: TEST_MODE ? "rinkeby" : "mainnet",
 };
 
