3641a841d635da9df3fd6a995a7334cb1bd01655
Chase Manning <me@chasemanning.co.nz>
2021-05-02 13:44:14 +1000

    :rocket: add capsule created confirmation popup

--- client/src/components/CreateCapsule.tsx
+++ client/src/components/CreateCapsule.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import BN from "bn.js";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { useHistory } from "react-router";
+
 import {
   createCapsule,
   getTokenContract,
@@ -33,19 +35,23 @@ type Props = {
 };
 
 const CreateCapsule = (props: Props): JSX.Element => {
+  if (!props.show) return <></>;
+
+  const history = useHistory();
+
   const ethAsset: Asset = { token: "ETH", value: "0" };
-  const [approving, setApproving] = useState(false);
+  const approval: Approval = { asset: ethAsset, approved: true };
+
+  const [loading, setLoading] = useState(false);
+  const [complete, setComplete] = useState(false);
   const [beneficiary, setBeneficiary] = useState("");
   const [distributionDate, setDistributionDate] = useState("");
   const [assets, setAssets] = useState<Asset[]>([ethAsset]);
-  const [approvals, setApprovals] = useState<Approval[]>([
-    { asset: ethAsset, approved: true },
-  ]);
+  const [approvals, setApprovals] = useState<Approval[]>([approval]);
+
   const tokens = useSelector(selectTokens);
 
-  const unapproved = approvals.filter(
-    (approval: Approval) => !approval.approved
-  );
+  const unapproved = approvals.filter((a: Approval) => !a.approved);
 
   const addressSymbol = (address: string) =>
     tokens.filter((token: Token) => token.address === address)[0].symbol;
@@ -68,74 +74,76 @@ const CreateCapsule = (props: Props): JSX.Element => {
       .approve(GLOBALS.CAPSULE, new BN("9999999999999999999999999999"))
       .send()
       .on("transactionHash", (hash: any) => {
-        setApproving(true);
+        setLoading(true);
       })
       .on("receipt", (receipt: any) => {
-        updateApprovals(assets).then(() => setApproving(false));
+        updateApprovals(assets).then(() => setLoading(false));
       })
       .on("error", (err: any) => {
         console.log(`Error: ${err}`);
-        setApproving(false);
+        setLoading(false);
       });
   };
 
-  const clearInputs = () => {
-    setBeneficiary("");
-    setDistributionDate("");
-    setAssets([ethAsset]);
-    setApprovals([{ asset: ethAsset, approved: true }]);
-    setApproving(false);
-  };
-
   const create = async () => {
+    setLoading(true);
     const date = inputToDate(distributionDate);
     await createCapsule(beneficiary, date, assets);
-    clearInputs();
   };
 
   return (
-    <Popup
-      show={props.show}
-      close={props.close}
-      header="Create Capsule"
-      content={
-        <Content>
-          <TextInput
-            label="Distribution Date"
-            placeholder="mm/dd/yyyy"
-            tooltip="This is the date when the capsule will be able to be opened"
-            value={distributionDate}
-            setValue={(value: string) => setDistributionDate(value)}
-          />
-          <TextInput
-            label="Beneficiary"
-            placeholder="e.g. 0x07d48BDBA7975f0DAF73BD5b85A2E3Ff87ffb24e"
-            tooltip="This is the wallet address that your crypto will be sent to on the distribution date"
-            value={beneficiary}
-            setValue={(value: string) => setBeneficiary(value)}
-          />
-          <Assets
-            assets={assets}
-            setAssets={(assets: Asset[]) => {
-              setAssets(assets);
-              updateApprovals(assets);
-            }}
-          />
-        </Content>
-      }
-      buttonText={
-        approving
-          ? "Loading"
-          : unapproved.length === 0
-          ? "Create"
-          : `Approve ${addressSymbol(unapproved[0].asset.token)}`
-      }
-      buttonAction={() => {
-        if (approving) return;
-        if (unapproved.length === 0) create();
-        else tokenApprove(unapproved[0].asset.token);
-      }}
-    />
+    <>
+      <Popup
+        show={!complete}
+        close={props.close}
+        header="Create Capsule"
+        content={
+          <Content>
+            <TextInput
+              label="Distribution Date"
+              placeholder="mm/dd/yyyy"
+              tooltip="This is the date when the capsule will be able to be opened"
+              value={distributionDate}
+              setValue={(value: string) => setDistributionDate(value)}
+            />
+            <TextInput
+              label="Beneficiary"
+              placeholder="e.g. 0x07d48BDBA7975f0DAF73BD5b85A2E3Ff87ffb24e"
+              tooltip="This is the wallet address that your crypto will be sent to on the distribution date"
+              value={beneficiary}
+              setValue={(value: string) => setBeneficiary(value)}
+            />
+            <Assets
+              assets={assets}
+              setAssets={(assets: Asset[]) => {
+                setAssets(assets);
+                updateApprovals(assets);
+              }}
+            />
+          </Content>
+        }
+        buttonText={
+          loading
+            ? "Loading"
+            : unapproved.length === 0
+            ? "Create"
+            : `Approve ${addressSymbol(unapproved[0].asset.token)}`
+        }
+        buttonAction={() => {
+          if (loading) return;
+          if (unapproved.length === 0) create();
+          else tokenApprove(unapproved[0].asset.token);
+        }}
+      />
+      <Popup
+        show={complete}
+        close={props.close}
+        header="Capsule Created!"
+        body="Your Capsule was successfully created, click below to view your capsule"
+        buttonText="View Capsule"
+        buttonAction={() => history.push("/sent")}
+      />
+    </>
   );
 };
 
