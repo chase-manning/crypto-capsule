8ed933a1b7c942b315e2592fb7eb5fdecf28d6de
Chase Manning <me@chasemanning.co.nz>
2021-05-09 16:10:19 +1200

    :wrench: update capsule contract

--- client/src/utils/globals.ts
+++ client/src/utils/globals.ts
@@ -6,7 +6,7 @@ type GlobalsType = {
 };
 
 const GLOBALS: GlobalsType = {
-  CAPSULE: "0x14430C6E97d76029B1de0926303F88d90669Ac40",
+  CAPSULE: "0x4611614a4e07906cc3Bb77353DEDF53B825b8553",
   NETWORK: TEST_MODE ? "rinkeby" : "mainnet",
 };
 
