43ce1ed3678092f3f33ad4d515ce121128fecd1c
Chase Manning <me@chasemanning.co.nz>
2021-05-10 21:02:22 +1200

    :sparkles: create label component

---
+++ client/src/components/Label.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import Tooltip from "./Tooltip";
+
+const LabelContainter = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.8rem;
+`;
+
+const StyledLabel = styled.label`
+  color: var(--main);
+  font-size: 17px;
+`;
+
+type Props = {
+  text: string;
+  tooltip?: string;
+};
+
+const Label = (props: Props): JSX.Element => {
+  return (
+    <LabelContainter>
+      <StyledLabel>{props.text}</StyledLabel>
+      {props.tooltip && <Tooltip content={props.tooltip} />}
+    </LabelContainter>
+  );
+};
+
+export default Label;
