ad317efc6725e195a3646b8117528beafc53f553
Chase Manning <me@chasemanning.co.nz>
2021-05-17 20:49:13 +1200

    :rocket: add decimals to oracle

--- contracts/oracle/Oracle.sol
+++ contracts/oracle/Oracle.sol
@@ -10,8 +10,10 @@ contract Oracle {
         uint80 answeredInRound;
     }
 
+    uint256 public decimals = 8;
+
     function latestRoundData() public pure returns(Data memory) {
-        Data memory data = Data(1,2,1,1,1);
+        Data memory data = Data(1,200000000,1,1,1);
         return data;
     }
 }
