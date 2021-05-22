ca7ed3779645a760a4e16c6c1a58ca7fd8c2b5ab
Chase Manning <me@chasemanning.co.nz>
2021-05-22 19:09:49 +1200

    :rocket: add slider to home page

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Landing from "./Landing";
+import Slider from "./Slider";
 
 const StyledHomePage = styled.div`
   display: flex;
@@ -11,6 +12,7 @@ const HomePage = (): JSX.Element => {
   return (
     <StyledHomePage>
       <Landing />
+      <Slider />
     </StyledHomePage>
   );
 };
