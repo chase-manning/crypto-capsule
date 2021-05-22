2920c7de1431d17d2d6d468ac53b5d798d894001
Chase Manning <me@chasemanning.co.nz>
2021-05-22 20:51:27 +1200

    :lipstick: change selector to use block

--- client/src/components/Selector.tsx
+++ client/src/components/Selector.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "./Button";
 import Label from "./Label";
 
 const StyledSelector = styled.div`
@@ -9,38 +10,6 @@ const StyledSelector = styled.div`
 
 const OptionsContainer = styled.div`
   display: flex;
-`;
-
-type OptionProps = {
-  active: boolean;
-  left?: boolean;
-  right?: boolean;
-};
-
-const Option = styled.button`
-  height: 3.5rem;
-  width: 12rem;
-  font-size: 1.4rem;
-  font-weight: 500;
-  margin-bottom: 1rem;
-  cursor: pointer;
-  text-transform: capitalize;
-  background-color: ${(props: OptionProps) =>
-    props.active ? "var(--main)" : "var(--bg)"};
-  color: ${(props: OptionProps) =>
-    props.active ? "var(--bg)" : "var(--main)"};
-  border: solid 1px var(--main);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-bottom-left-radius: ${(props: OptionProps) =>
-    props.left ? "1.7rem" : "0"};
-  border-top-left-radius: ${(props: OptionProps) =>
-    props.left ? "1.7rem" : "0"};
-  border-bottom-right-radius: ${(props: OptionProps) =>
-    props.right ? "1.7rem" : "0"};
-  border-top-right-radius: ${(props: OptionProps) =>
-    props.right ? "1.7rem" : "0"};
 `;
 
 type Props = {
@@ -57,15 +26,13 @@ const Selector = (props: Props): JSX.Element => {
       <Label text={props.label} tooltip={props.tooltip} />
       <OptionsContainer>
         {props.options.map((option: string, index: number) => (
-          <Option
+          <Button
+            small
             key={option}
-            active={option === props.activeOption}
-            left={index === 0}
-            right={index === props.options.length - 1}
-            onClick={() => props.setOption(option)}
-          >
-            {option}
-          </Option>
+            primary={option === props.activeOption}
+            text={option}
+            click={() => props.setOption(option)}
+          />
         ))}
       </OptionsContainer>
     </StyledSelector>
