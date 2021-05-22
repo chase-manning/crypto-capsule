f4a8ef1ff0684f9be1c3ac4e85e4a2e1735113fa
Chase Manning <me@chasemanning.co.nz>
2021-05-23 09:04:25 +1200

    :bug: selector position

--- client/src/components/Selector.tsx
+++ client/src/components/Selector.tsx
@@ -8,8 +8,14 @@ const StyledSelector = styled.div`
   flex-direction: column;
 `;
 
+const Container = styled.div`
+  display: flex;
+  margin: 1rem;
+`;
+
 const OptionsContainer = styled.div`
   display: flex;
+  flex-direction: row-reverse;
 `;
 
 type Props = {
@@ -24,17 +30,19 @@ const Selector = (props: Props): JSX.Element => {
   return (
     <StyledSelector>
       <Label text={props.label} tooltip={props.tooltip} />
-      <OptionsContainer>
-        {props.options.map((option: string, index: number) => (
-          <Button
-            small
-            key={option}
-            primary={option === props.activeOption}
-            text={option}
-            click={() => props.setOption(option)}
-          />
-        ))}
-      </OptionsContainer>
+      <Container>
+        <OptionsContainer>
+          {props.options.map((option: string, index: number) => (
+            <Button
+              small
+              key={option}
+              selected={option === props.activeOption}
+              text={option}
+              click={() => props.setOption(option)}
+            />
+          ))}
+        </OptionsContainer>
+      </Container>
     </StyledSelector>
   );
 };
