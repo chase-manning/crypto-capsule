b5f18af5e3fc8639a8dcdec68e4d4db76404bca1
Chase Manning <me@chasemanning.co.nz>
2021-05-22 17:52:01 +1200

    :sparkles: add spacing option to validation erro

--- client/src/styles/ValidationError.tsx
+++ client/src/styles/ValidationError.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
+type Props = {
+  spacing?: boolean;
+};
+
 export const ValidationError = styled.div`
   font-size: 1.2rem;
   color: var(--danger);
+  margin-top: ${(props: Props) => (props.spacing ? "1rem" : "0")};
 `;
