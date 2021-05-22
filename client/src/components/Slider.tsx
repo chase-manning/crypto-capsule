6b7a4ec86adaccc67a1331c3c7b4da8ec89039d8
Chase Manning <me@chasemanning.co.nz>
2021-05-23 10:40:22 +1200

    :rocket: create what is slider

--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import dots from "../assets/dots.png";
+import Block from "./Block";
+import Button from "./Button";
 
 const StyledSlider = styled.div`
   position: relative;
   width: 100%;
-  padding: 10rem;
-  height: 60rem;
+  padding: 13rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Gradient = styled.div`
@@ -30,12 +34,91 @@ const Dots = styled.div`
   opacity: 0.9;
 `;
 
+const Container = styled.div`
+  position: relative;
+  width: 60vw;
+  padding: 5rem;
+`;
+
+const Content = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.h2`
+  color: var(--main);
+  font-size: 7rem;
+  font-weight: 600;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 3rem;
+`;
+
+const Body = styled.p`
+  color: var(--sub);
+  font-size: 2rem;
+  font-weight: 500;
+  width: 100%;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin-top: 5rem;
+
+  button {
+    margin: 0 2rem;
+  }
+`;
+
 const Slider = (): JSX.Element => {
   return (
     <StyledSlider>
       <Dots />
       <Gradient />
-      meow
+      <Container>
+        <Block />
+        <Content>
+          <Header>What is Crypto Capsule?</Header>
+          <Body>
+            Crypto Capsule is a free tool for sending crypto to yourself or
+            someone else that can only be accessed at a specified time in the
+            future. When creating a Capsule you can choose several options to
+            customise when the Capsule opens and how the crypto is distributed.
+            Crypto Capsule uses an Ethereum Smart Contract to store the crypto
+            while it is in the Capsule and for retrieving the crypto when it is
+            opened.
+          </Body>
+          <ButtonContainer>
+            <Button
+              primary
+              text="View Smart Contract"
+              click={() =>
+                (window as any)
+                  .open(
+                    "https://etherscan.io/address/0x07d48bdba7975f0daf73bd5b85a2e3ff87ffb24e",
+                    "_blank"
+                  )
+                  .focus()
+              }
+            />
+            <Button
+              primary
+              text="View Source Code"
+              click={() =>
+                (window as any)
+                  .open(
+                    "https://github.com/chase-manning/crypto-capsule/blob/master/contracts/Capsule.sol",
+                    "_blank"
+                  )
+                  .focus()
+              }
+            />
+          </ButtonContainer>
+        </Content>
+      </Container>
     </StyledSlider>
   );
 };
