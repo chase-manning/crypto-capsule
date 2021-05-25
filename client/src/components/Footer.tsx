90c818e080889ee16f6cc59a6c5b0792d0e611d0
Chase Manning <me@chasemanning.co.nz>
2021-05-25 18:57:00 +1200

    :rocket: add buttons to footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,37 +1,29 @@
 import React from "react";
-import { useHistory } from "react-router";
 import styled from "styled-components";
+import Button from "./Button";
 
-type Button = {
-  label: string;
-  route: string;
-};
-
-type Link = {
+type ButtonType = {
   label: string;
   link: string;
 };
 
-const buttons: Button[] = [
-  {
-    label: "Audit",
-    route: "/sent",
-  },
-];
-
-const links: Link[] = [
-  {
-    label: "Twitter",
-    link: "xxx",
-  },
-  {
-    label: "Discord",
-    link: "xxx",
-  },
-  {
-    label: "GitHub",
-    link: "xxx",
-  },
+const buttons: ButtonType[][] = [
+  [
+    {
+      label: "Twitter",
+      link: "xxx",
+    },
+  ],
+  [
+    {
+      label: "Discord",
+      link: "xxx",
+    },
+    {
+      label: "GitHub",
+      link: "xxx",
+    },
+  ],
 ];
 
 const StyledFooter = styled.div`
@@ -40,69 +32,29 @@ const StyledFooter = styled.div`
   display: flex;
   justify-content: space-between;
   position: relative;
-  margin-top: 3rem;
 `;
 
 const Section = styled.div`
   display: flex;
-`;
 
-const Link = styled.a`
-  text-decoration: none;
-  color: var(--main);
-  font-size: 1.4rem;
-  margin: 0 2rem;
-
-  :hover {
-    text-decoration: underline;
+  button {
+    margin: 0 1rem;
   }
-`;
-
-const Button = styled.button`
-  text-decoration: none;
-  color: var(--main);
-  font-size: 1.4rem;
-  margin: 0 2rem;
-
-  :hover {
-    text-decoration: underline;
-  }
-`;
-
-const Text = styled.div`
-  font-size: 1.4rem;
-  color: var(--main);
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
 `;
 
 const Footer = (): JSX.Element => {
-  const history = useHistory();
-
   return (
     <StyledFooter>
-      <Section>
-        {buttons.map((button: Button) => (
-          <Button onClick={() => history.push(button.route)} key={button.label}>
-            {button.label}
-          </Button>
-        ))}
-      </Section>
-      <Text>Made with ❤️in New Zealand</Text>
-      <Section>
-        {links.map((link: Link) => (
-          <Link
-            href={link.link}
-            target="_blank"
-            rel="noreferrer"
-            key={link.label}
-          >
-            {link.label}
-          </Link>
-        ))}
-      </Section>
+      {buttons.map((section: ButtonType[]) => (
+        <Section>
+          {section.map((button: ButtonType) => (
+            <Button
+              text={button.label}
+              click={() => (window as any).open(button.link, "_blank").focus()}
+            />
+          ))}
+        </Section>
+      ))}
     </StyledFooter>
   );
 };
