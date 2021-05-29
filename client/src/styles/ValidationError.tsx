2892916e67af0109cb5e1f7ca749468762cd8995
Chase Manning <me@chasemanning.co.nz>
2021-05-29 16:19:19 +1200

    :lipstick: increase validation error font size

--- client/src/styles/ValidationError.tsx
+++ client/src/styles/ValidationError.tsx
@@ -5,7 +5,8 @@ type Props = {
 };
 
 export const ValidationError = styled.div`
-  font-size: 1.2rem;
+  font-size: 1.7rem;
   color: var(--danger);
   margin-top: ${(props: Props) => (props.spacing ? "1rem" : "0")};
+  margin-bottom: 1rem;
 `;
