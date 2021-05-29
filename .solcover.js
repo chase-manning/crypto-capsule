bbe025f5d101c948c1da7c3bc977bbf0a6a42dd7
Chase Manning <me@chasemanning.co.nz>
2021-05-30 10:08:09 +1200

    :fire: remove redundant sol cover files

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,5 @@
 module.exports = {
   skipFiles: [
-    "CapsuleCoin.sol",
     "token/ERC20.sol",
     "token/TestERC20.sol",
     "utils/Address.sol",
@@ -8,9 +7,6 @@ module.exports = {
     "utils/EnumerableSet.sol",
     "utils/Ownable.sol",
     "utils/Initializable.sol",
-    "extensions/IERC20Metadata.sol",
-    "interfaces/AggregatorV3Interface.sol",
     "interfaces/IERC20",
-    "oracle/Oracle.sol",
   ],
 };
