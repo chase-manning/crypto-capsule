f34c92ed24bc38d9c05dd7dd71843d4815cb2bb9
Chase Manning <me@chasemanning.co.nz>
2021-05-30 20:14:07 +1200

    :rocket: add blue background to why use slide

--- client/src/components/WhyUseSlide.tsx
+++ client/src/components/WhyUseSlide.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Note from "./Note";
+import noise from "../assets/noise.png";
 
 const StyledWhyUseSlide = styled.div`
   position: relative;
@@ -17,23 +18,31 @@ const Gradient = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: linear-gradient(145deg, var(--primary), var(--bg));
-  opacity: 0.4;
+  background: linear-gradient(
+    90deg,
+    var(--bg),
+    var(--sub),
+    var(--bg),
+    var(--bg)
+  );
+  opacity: 0.5;
 `;
 
-const Dots = styled.div`
+const Noise = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-size: 7px 7px;
-  opacity: 0.9;
+  background-image: url(${noise});
+  background-size: 5px 5px;
+  opacity: 1;
 `;
 
 const WhyUseSlide = (): JSX.Element => {
   return (
     <StyledWhyUseSlide>
+      <Noise />
       <Gradient />
       <Note
         header="Why Use Crypto Capsule?"
