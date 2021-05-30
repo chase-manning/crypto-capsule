a94a7ce6b8d89bacda87068ff022c1521f56c07a
Chase Manning <me@chasemanning.co.nz>
2021-05-30 19:43:39 +1200

    :art: change what is slide to use note component

--- client/src/components/WhatIsSlide.tsx
+++ client/src/components/WhatIsSlide.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import dots from "../assets/dots.png";
-import Block from "./Block";
+import Note from "./Note";
 
-const StyledSlider = styled.div`
+const StyledWhatIsSlide = styled.div`
   position: relative;
   width: 100%;
   padding: 12rem;
@@ -33,55 +33,18 @@ const Dots = styled.div`
   opacity: 0.9;
 `;
 
-const Container = styled.div`
-  position: relative;
-  width: 60vw;
-  padding: 5rem;
-  padding-top: 3.3rem;
-  transform: rotate(-3deg);
-`;
-
-const Content = styled.div`
-  position: relative;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Header = styled.h2`
-  color: var(--main);
-  font-size: 8rem;
-  font-weight: 600;
-  width: 100%;
-  text-align: center;
-  margin-bottom: 3rem;
-`;
-
-const Body = styled.p`
-  color: var(--sub);
-  font-size: 3rem;
-  font-weight: 500;
-  width: 100%;
-`;
-
 const WhatIsSlide = (): JSX.Element => {
   return (
     <StyledSlider>
       <Dots />
       <Gradient />
-      <Container>
-        <Block />
-        <Content>
-          <Header>What is Crypto Capsule?</Header>
-          <Body>
-            Crypto Capsule is a free tool for sending crypto to yourself or
+      <Note
+        header="What is Crypto Capsule?"
+        body="Crypto Capsule is a free tool for sending crypto to yourself or
             someone else that can only be accessed at a specified time in the
             future. When creating a Capsule you can choose several options to
-            customise when the Capsule opens and how the crypto is distributed.
-          </Body>
-        </Content>
-      </Container>
+            customise when the Capsule opens and how the crypto is distributed."
+      />
     </StyledSlider>
   );
 };
