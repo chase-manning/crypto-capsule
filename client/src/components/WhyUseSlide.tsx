712284e781eda420d77516008e873a440a916873
Chase Manning <me@chasemanning.co.nz>
2021-05-30 22:56:04 +1200

    :rocket: add sections to why use slide

--- client/src/components/WhyUseSlide.tsx
+++ client/src/components/WhyUseSlide.tsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Note from "./Note";
 import noise from "../assets/noise.png";
+import Button from "./Button";
+
+type SectionContent = {
+  header: string;
+  body: string;
+  button: string;
+};
+
+const sections: SectionContent[] = [
+  {
+    header: "Why Use Crypto Capsule?",
+    body:
+      "Crypto Capsule is super versitile! Click through to see some example use cases <3",
+    button: "Show Me!",
+  },
+  {
+    header: "Escrow Employee Tokens",
+    body:
+      "Sometimes a protocol will want to distribute their native tokens to their employees or developers. To avoid the risk of employees selling all tokens on distribution, it is sometimes desirable to have these escrowed and gradually distributed over time.",
+    button: "Tell me More...",
+  },
+  {
+    header: "Trustless Trust Fund",
+    body:
+      "Crypto Capsule can be used as a trust fund to distribute assets to a beneficiary. This could be used to build a trust fund for your children that distributes to them when they turn 21 years of age.",
+    button: "So cool!!!",
+  },
+  {
+    header: "Lock LP Tokens",
+    body:
+      "Sometimes a Protocol will allocate a portion of minted tokens to providing liquidity. These are often locked for a period of time to give users confidence that there will be liquidity. Crypto Capsule can be used to lock these LP Tokens and share the capsule with users as proof.",
+    button: "No Way :O",
+  },
+  {
+    header: "Crypto Term Deposit",
+    body:
+      "Use Crypto Capsule as a term deposit, top up when you want to add to your savings, and set the capsule to open when you want to withdraw. Great for dollar cost averaging in and out for the next bull run.",
+    button: "Epic <3",
+  },
+];
 
 const StyledWhyUseSlide = styled.div`
   position: relative;
   width: 100%;
-  padding: 13rem;
+  height: 55rem;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
 `;
 
 const Gradient = styled.div`
@@ -39,15 +81,54 @@ const Noise = styled.div`
   opacity: 1;
 `;
 
+type SectionProps = {
+  position: number;
+};
+
+const Section = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transform-origin: 50% 1000%;
+  transform: translate(-50%, -45%)
+    rotate(${(props: SectionProps) => props.position * 30}deg);
+  transition: transform 1s;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 3rem;
+  width: 60vw;
+  height: 4rem;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 3rem;
+  transform: rotate(-1deg);
+`;
+
 const WhyUseSlide = (): JSX.Element => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
   return (
     <StyledWhyUseSlide>
       <Noise />
       <Gradient />
-      <Note
-        header="Why Use Crypto Capsule?"
-        body="Crypto Capsule is super versitile! Click through to see some example use cases <3"
-      />
+      {sections.map((section: SectionContent, index: number) => (
+        <Section position={index - slideIndex}>
+          <Note header={section.header} body={section.body} />
+          <ButtonContainer>
+            {index !== sections.length - 1 && (
+              <Button
+                text={section.button}
+                click={() => setSlideIndex(index + 1)}
+              />
+            )}
+          </ButtonContainer>
+        </Section>
+      ))}
     </StyledWhyUseSlide>
   );
 };
