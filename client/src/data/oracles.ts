9e260500fd08c39f780ed3faee686a3f0c536f3e
Chase Manning <me@chasemanning.co.nz>
2021-06-01 21:46:29 +1200

    :sparkles: add oracles data

---
+++ client/src/data/oracles.ts
@@ -0,0 +1,28 @@
+import { TEST_MODE } from "../utils/globals";
+
+export type OracleType = {
+  token: string;
+  oracle: string;
+};
+
+const ORACLES: OracleType[] = TEST_MODE
+  ? [
+      {
+        token: "btc",
+        oracle: "0xECe365B379E1dD183B20fc5f022230C044d51404",
+      },
+      {
+        token: "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735",
+        oracle: "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF",
+      },
+
+      {
+        token: "ETH",
+        oracle: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+      },
+    ]
+  : [];
+
+export default ORACLES;
+
+// https://docs.chain.link/docs/ethereum-addresses/
