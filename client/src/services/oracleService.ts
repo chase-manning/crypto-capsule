008ad3a15d5b82efdec210f995c7dcb9e50bb008
Chase Manning <me@chasemanning.co.nz>
2021-06-01 22:23:13 +1200

    :fire: remove logging

--- client/src/services/oracleService.ts
+++ client/src/services/oracleService.ts
@@ -16,24 +16,17 @@ export const getOracleContract = async (oracle: string): Promise<Contract> => {
 };
 
 export const getOracle = (token: string): string => {
-  console.log("Getting oracle");
   const oracles = ORACLES.filter((o: OracleType) => o.token === token);
-  console.log(oracles);
   if (oracles.length === 0) return "";
   return oracles[0].oracle;
 };
 
 export const getTokenPriceInUsd = async (token: string): Promise<number> => {
-  console.log("Getting token price in usd");
   const oracle = getOracle(token);
-  console.log(oracle);
-  console.log("Before error");
   const oracleContract = await getOracleContract(oracle);
-  console.log("Got conract");
   const roundData: RoundDataType = await oracleContract.methods
     .latestRoundData()
     .call();
-  console.log(roundData);
   return roundData.answer / 10 ** 8;
 };
 
@@ -41,18 +34,13 @@ export const getTokenValueInUsd = async (
   token: string,
   amount: number
 ): Promise<number> => {
-  console.log("Token Value in usd");
   const price = await getTokenPriceInUsd(token);
-  console.log(price);
-  console.log(amount);
-  console.log(price * amount);
   return amount * price;
 };
 
 export const getCapsuleUsdValue = async (
   capsule: CapsuleType
 ): Promise<number> => {
-  console.log("getting capsule usd value");
   const usds: number[] = [];
   const promises = capsule.assets.map(async (asset: Asset) =>
     usds.push(
