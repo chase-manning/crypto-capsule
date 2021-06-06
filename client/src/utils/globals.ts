bf7e501da370028201a72e236008ddb92db3a052
Chase Manning <me@chasemanning.co.nz>
2021-06-06 16:43:20 +1200

    :bug: capsules not creating

--- client/src/utils/globals.ts
+++ client/src/utils/globals.ts
@@ -6,7 +6,7 @@ type GlobalsType = {
 };
 
 const GLOBALS: GlobalsType = {
-  CAPSULE: "0x9F37d5DE3F48E8af0DEB8023e940fE952FfffE5B",
+  CAPSULE: "0x653C428093EDa1BE5d634e3F229585856002B4d4",
   NETWORK: TEST_MODE ? "rinkeby" : "mainnet",
 };
 
