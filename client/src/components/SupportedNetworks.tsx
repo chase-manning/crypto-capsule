2659c67de1259116c22237c10218439129a0c460
Chase Manning <me@chasemanning.co.nz>
2021-06-06 18:08:19 +1200

    :sparkles: create supported networks component

---
+++ client/src/components/SupportedNetworks.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSupportedNetworks = styled.div`
+  width: 100%;
+  padding: 3rem;
+`;
+
+const SupportedNetworks = (): JSX.Element => {
+  return <StyledSupportedNetworks>meow</StyledSupportedNetworks>;
+};
+
+export default SupportedNetworks;
