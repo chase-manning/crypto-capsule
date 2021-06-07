8607f90dd05bf3fdc8982b8d970c604a45c209d7
Chase Manning <me@chasemanning.co.nz>
2021-06-07 19:43:21 +1200

    :fire: remove capsule sections

--- client/src/components/Capsules.tsx
+++ client/src/components/Capsules.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import Title from "../styles/Title";
 import CapsuleType from "../types/CapsuleType";
 import Capsule from "./Capsule";
 
@@ -10,53 +9,25 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const GetCapsules = (capsuleList: CapsuleType[]) =>
-  capsuleList
-    .sort(
-      (a: CapsuleType, b: CapsuleType) =>
-        new Date(a.distributionDate).getTime() -
-        new Date(b.distributionDate).getTime()
-    )
-    .map((capsule: CapsuleType, index: number) => (
-      <Capsule key={index} capsule={capsule} />
-    ));
-
 type Props = {
   capsules: CapsuleType[];
 };
 
 const Capsules = (props: Props): JSX.Element => {
-  const now = new Date();
-  const ready = props.capsules.filter(
-    (cap: CapsuleType) => new Date(cap.distributionDate) < now && !cap.empty
-  );
-  const upcoming = props.capsules.filter(
-    (cap: CapsuleType) => new Date(cap.distributionDate) >= now
-  );
-  const opened = props.capsules.filter(
-    (cap: CapsuleType) => new Date(cap.distributionDate) < now && cap.empty
-  );
-
   return (
     <Container>
-      {ready.length > 0 && (
-        <>
-          <Title>Ready</Title>
-          {GetCapsules(ready)}
-        </>
-      )}
-      {upcoming.length > 0 && (
-        <>
-          <Title>Upcoming</Title>
-          {GetCapsules(upcoming)}
-        </>
-      )}
-      {opened.length > 0 && (
-        <>
-          <Title>Empty</Title>
-          {GetCapsules(opened)}
-        </>
-      )}
+      {props.capsules
+        .sort(
+          (a: CapsuleType, b: CapsuleType) =>
+            a.distributionDate.getTime() - b.distributionDate.getTime()
+        )
+        .sort(
+          (a: CapsuleType, b: CapsuleType) =>
+            (a.empty ? 1 : 0) - (b.empty ? 1 : 0)
+        )
+        .map((capsule: CapsuleType) => (
+          <Capsule capsule={capsule} />
+        ))}
     </Container>
   );
 };
